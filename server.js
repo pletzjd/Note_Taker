b781eb3eab33a5d7956dac0d7892e07ee39d742b
pletzjd <j.pletzer17@gmail.com>
2022-06-18 00:07:57 -0400

    Server now handles api get request for notes database

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const readFile = util.promisify(fs.readFile);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+// Get request for saved notes
+app.get("/api/notes", (req, res) =>
+    readFile("./db/db.json")
+    
+    .then((data) => 
+        res.json(JSON.parse(data))
+));
+
 
 // GET Route for homepage
 app.get('/', (req, res) =>
