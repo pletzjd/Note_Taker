a058291c1ecaf3ff12dae837a8ca2650b385cc07
pletzjd <j.pletzer17@gmail.com>
2022-06-17 21:50:42 -0400

    Can now navigate to notes.html from index.html

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
