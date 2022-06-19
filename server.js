306bc094e7eabf04ebf784f401eee226d705ef9e
pletzjd <j.pletzer17@gmail.com>
2022-06-19 00:08:13 -0400

    Added wildcard route

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//Wild card get route
+//Wildcard get route
+app.get("*", (req,res)=>
+  res.sendFile(path.join(__dirname,'/public/index.html'))
+)
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
